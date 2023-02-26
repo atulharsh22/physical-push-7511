cdfd186b6fc7e373efc457d50ab201c163c9165f
atulharsh22 <atulharsh22@gmail.com>
2023-02-26 21:16:33 +0530

    updated

--- Online Hardware and software support system/src/com/usecases/Main.java
+++ Online Hardware and software support system/src/com/usecases/Main.java
@@ -72,96 +72,109 @@ public class Main {
 		}
 		else if(N==2)
 		{
+			while(true)
+			{
+				
+				System.out.println("Enter 1 to register ");
+				System.out.println("Enter 2 to login ");
+				System.out.println("Enter 3 to register complain ");
+				System.out.println("Enter 4 to see status of complaint ");
+				System.out.println("Enter 5 to see all complaints raised");
+				System.out.println("Enter 6 to change password");
+				
+				System.out.println();
+				
+				int choice = sc.nextInt();
+				
+				if(choice == 1)
+				{
+					EmployeeRegistration reg = new EmployeeRegistration();
+					reg.employeeRegistration();
+				}
+				else if(choice == 2)
+				{
+					EmployeeLogin login = new EmployeeLogin();
+					login.employeeLogin();
+				}
+				else if(choice == 3)
+				{
+					EmployeeRaiseComplain comp = new EmployeeRaiseComplain();
+					comp.employeeRaiseComplain();
+				}
+				else if(choice == 4)
+				{
+					EmployeeCheckComplaintStatus status = new EmployeeCheckComplaintStatus();
+					status.employeeCheckComplaintStatus();
+				}
+				else if(choice == 5)
+				{
+					EmployeeCheckComplaintHistory allComp = new EmployeeCheckComplaintHistory();
+					allComp.employeeCheckComplaintHistory();
+				}
+				else if(choice == 6)
+				{
+					EmployeeChangePassword change = new EmployeeChangePassword();
+					change.employeeChangePassword();
+				}
+				else
+				{
+					System.out.println("Invalid input ...");
+				}
+				
+				
+			}
 			
-			System.out.println("Enter 1 to register ");
-			System.out.println("Enter 2 to login ");
-			System.out.println("Enter 3 to register complain ");
-			System.out.println("Enter 4 to see status of complaint ");
-			System.out.println("Enter 5 to see all complaints raised");
-			System.out.println("Enter 6 to change password");
-			
-			System.out.println();
-			
-			int choice = sc.nextInt();
-			
-			if(choice == 1)
-			{
-				EmployeeRegistration reg = new EmployeeRegistration();
-				reg.employeeRegistration();
-			}
-			else if(choice == 2)
-			{
-				EmployeeLogin login = new EmployeeLogin();
-				login.employeeLogin();
-			}
-			else if(choice == 3)
-			{
-				EmployeeRaiseComplain comp = new EmployeeRaiseComplain();
-				comp.employeeRaiseComplain();
-			}
-			else if(choice == 4)
-			{
-				EmployeeCheckComplaintStatus status = new EmployeeCheckComplaintStatus();
-				status.employeeCheckComplaintStatus();
-			}
-			else if(choice == 5)
-			{
-				EmployeeCheckComplaintHistory allComp = new EmployeeCheckComplaintHistory();
-				allComp.employeeCheckComplaintHistory();
-			}
-			else if(choice == 6)
-			{
-				EmployeeChangePassword change = new EmployeeChangePassword();
-				change.employeeChangePassword();
-			}
-			else
-			{
-				System.out.println("Invalid input ...");
-			}
+
 			
 		}
 		else if(N==3)
 		{
+			while(true)
+			{
+				
+				System.out.println("Enter 1 to login");
+				System.out.println("Enter 2 to view problems assigned");
+				System.out.println("Enter 3 to update status of problem addressed");
+				System.out.println("Enter 4 to see list of problems attended");
+				System.out.println("Enter 5 to change password");
+				
+				System.out.println();
+				
+				int choice = sc.nextInt();
+				
+				if(choice == 1)
+				{
+					EngineerLogin login = new EngineerLogin();
+					login.engineerLogin();
+				}
+				else if(choice == 2)
+				{
+					EngineerCheckComplaintsAssigned comp = new EngineerCheckComplaintsAssigned();
+					comp.engineerCheckComplaintsAssigned();
+				}
+				else if(choice == 3)
+				{
+					EngineerUpdateComplaintStatus status = new EngineerUpdateComplaintStatus();
+					status.engineerUpdateComplaintStatus();
+				}
+				else if(choice == 4)
+				{
+					EngineerCheckAttendedComplaints attended = new EngineerCheckAttendedComplaints();
+					attended.engineerCheckAttendedComplaints();
+				}
+				else if(choice == 5)
+				{
+					EngineerChangePassword change = new EngineerChangePassword();
+					change.engineerChangePassword();
+				}
+				else
+				{
+					System.out.println("Invalid input ...");
+				}
+				
+			}
 			
-			System.out.println("Enter 1 to login");
-			System.out.println("Enter 2 to view problems assigned");
-			System.out.println("Enter 3 to update status of problem addressed");
-			System.out.println("Enter 4 to see list of problems attended");
-			System.out.println("Enter 5 to change password");
-			
-			System.out.println();
-			
-			int choice = sc.nextInt();
-			
-			if(choice == 1)
-			{
-				EngineerLogin login = new EngineerLogin();
-				login.engineerLogin();
-			}
-			else if(choice == 2)
-			{
-				EngineerCheckComplaintsAssigned comp = new EngineerCheckComplaintsAssigned();
-				comp.engineerCheckComplaintsAssigned();
-			}
-			else if(choice == 3)
-			{
-				EngineerUpdateComplaintStatus status = new EngineerUpdateComplaintStatus();
-				status.engineerUpdateComplaintStatus();
-			}
-			else if(choice == 4)
-			{
-				EngineerCheckAttendedComplaints attended = new EngineerCheckAttendedComplaints();
-				attended.engineerCheckAttendedComplaints();
-			}
-			else if(choice == 5)
-			{
-				EngineerChangePassword change = new EngineerChangePassword();
-				change.engineerChangePassword();
-			}
-			else
-			{
-				System.out.println("Invalid input ...");
-			}
+
 			
 		}
 		else
